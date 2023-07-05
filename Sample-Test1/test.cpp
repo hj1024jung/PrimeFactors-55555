dbf9ef47e3888a462c660ab3b6abd78630285811
hj1024.jung <flksignal@gmail.com>
2023-07-05 14:24:48 +0900

    1st commit

---
+++ Sample-Test1/test.cpp
@@ -0,0 +1,13 @@
+#include "pch.h"
+#include "../Project2/prime_factor.cpp"
+
+TEST(TestSuiteBase, AlwaysPass)
+{
+	EXPECT_EQ(1, 1);
+}
+
+TEST(TC_Prime, p1)
+{
+	PrimeFactor p;
+	EXPECT_EQ(1, 1);
+}
